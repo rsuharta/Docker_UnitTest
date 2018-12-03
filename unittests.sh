88776a1da0b4429ed5a0e378b4a0077f1f1b2789
rsuharta <rsuharta@ferrontech.com>
2018-12-03 13:52:49 +1100

    Update unittests.sh

--- unittests.sh
+++ unittests.sh
@@ -4,4 +4,6 @@ docker build --no-cache ./src -f ./src/unittests.Dockerfile  -t "project:unittes
 
 # Allow build agent to perform clean build (removing the test results folder below)
 mkdir -m777 "$(pwd)"/UnitTestResults
+
+# The volume (-v) option mounts the test result (*.trx) file back to the host so that the build agent can read and display the test result
 docker run --rm -v "$(pwd)"/UnitTestResults:/test/UnitTests/TestResults project:unittest
